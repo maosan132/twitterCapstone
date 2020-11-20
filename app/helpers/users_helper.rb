a40fd1b57161f154014d00861cf830b3c2d7c44b
maosan132 <maosan132@gmail.com>
2020-11-20 16:14:10 -0500

    refactor user helper so a rubocop offense stopped showing, then fix all offenses

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,19 @@
 module UsersHelper
-  def assign_images(user, photo = true)
-    if photo && user.photo.attached?
+  def assign_images(user)
+    if user.photo.attached?
       image_tag user.photo
-    elsif photo
+    else
       user.photo.attach(io: File.open('app/assets/images/placeholder-photo.png'),
                         filename: 'placeholder-photo.png', content_type: 'image/png')
-    elsif user.coverimage.attached?
+    end
+  end
+
+  def assign_coverimage(user)
+    if user.coverimage.attached?
       image_tag user.coverimage
     else
       user.coverimage.attach(io: File.open('app/assets/images/placeholder-cover.png'),
-                             filename: 'placeholder-cover.png', content_type: 'image/png')
+                            filename: 'placeholder-cover.png', content_type: 'image/png')
     end
   end
 end
